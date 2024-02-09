a7105854b47bc37d5a31d1cefe4959a357e0f8c7
santicano <canosantiago404@gmail.com>
2024-02-08 21:46:22 -0300

    cambiado color border

--- src/renderer/src/components/DraggableTopBar.tsx
+++ src/renderer/src/components/DraggableTopBar.tsx
@@ -1,6 +1,6 @@
 export const DraggableTopBar = () => {
   return (
-    <header className="absolute inset-0 h-8 bg-transparent border-b-2 border-b-red-500">
+    <header className="absolute inset-0 h-8 bg-transparent border-b border-b-white/20">
       <nav className="flex items-center justify-end align-middle h-full">
         <button className="bg-transparent px-5 h-full hover:bg-slate-700/75">-</button>
         <button className="bg-transparent px-5 h-full hover:bg-slate-700/75">+</button>
